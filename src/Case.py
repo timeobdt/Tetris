09ceeae92bc5c8a71cd7d6b58d5c46b0b353cd59
timeobdt <timeo561@gmail.com>
2023-10-23 20:02:40 +0200

    add margin and centering

--- src/Case.py
+++ src/Case.py
@@ -1,8 +1,10 @@
 from Sprite import Sprite
-from constant import CASE_SIZE
+from constant import CASE_SIZE, GRID_POSITION_X, GRID_POSITION_Y
 
 
 class Case(Sprite):
 
     def __init__(self, color: str, row: int, column: int):
-        super().__init__(f'assets/{color}.jpg', column * CASE_SIZE, row * CASE_SIZE)
+        super().__init__(f'assets/{color}.jpg', column * CASE_SIZE + GRID_POSITION_X , row * CASE_SIZE + GRID_POSITION_Y) 
+        
+        
